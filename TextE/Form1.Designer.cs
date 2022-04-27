14ec057ddd34a909454a74ab6f5c683c72d77341
Leila H <104355619+LeiDosE@users.noreply.github.com>
2022-04-27 21:37:32 +0300

    Add files via upload

---
+++ TextE/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace TextE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveAsStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CopyStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.PasteStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CutStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.InfoStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CopyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.PasteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 422);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileStripMenuItem1,
+            this.EditStripMenuItem1,
+            this.FontStripMenuItem1,
+            this.InfoStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            // 
+            // FileStripMenuItem1
+            // 
+            this.FileStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenStripMenuItem1,
+            this.SaveStripMenuItem1,
+            this.SaveAsStripMenuItem1});
+            this.FileStripMenuItem1.Name = "FileStripMenuItem1";
+            this.FileStripMenuItem1.Size = new System.Drawing.Size(59, 24);
+            this.FileStripMenuItem1.Text = "Файл";
+            // 
+            // OpenStripMenuItem1
+            // 
+            this.OpenStripMenuItem1.Name = "OpenStripMenuItem1";
+            this.OpenStripMenuItem1.Size = new System.Drawing.Size(192, 26);
+            this.OpenStripMenuItem1.Text = "Открыть";
+            this.OpenStripMenuItem1.Click += new System.EventHandler(this.OpenStripMenuItem1_Click);
+            // 
+            // SaveStripMenuItem1
+            // 
+            this.SaveStripMenuItem1.Name = "SaveStripMenuItem1";
+            this.SaveStripMenuItem1.Size = new System.Drawing.Size(192, 26);
+            this.SaveStripMenuItem1.Text = "Сохранить";
+            this.SaveStripMenuItem1.Click += new System.EventHandler(this.SaveStripMenuItem1_Click);
+            // 
+            // SaveAsStripMenuItem1
+            // 
+            this.SaveAsStripMenuItem1.Name = "SaveAsStripMenuItem1";
+            this.SaveAsStripMenuItem1.Size = new System.Drawing.Size(192, 26);
+            this.SaveAsStripMenuItem1.Text = "Сохранить как";
+            this.SaveAsStripMenuItem1.Click += new System.EventHandler(this.SaveAsStripMenuItem1_Click);
+            // 
+            // EditStripMenuItem1
+            // 
+            this.EditStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CopyStripMenuItem1,
+            this.PasteStripMenuItem1,
+            this.CutStripMenuItem1});
+            this.EditStripMenuItem1.Name = "EditStripMenuItem1";
+            this.EditStripMenuItem1.Size = new System.Drawing.Size(74, 24);
+            this.EditStripMenuItem1.Text = "Правка";
+            // 
+            // CopyStripMenuItem1
+            // 
+            this.CopyStripMenuItem1.Name = "CopyStripMenuItem1";
+            this.CopyStripMenuItem1.Size = new System.Drawing.Size(176, 26);
+            this.CopyStripMenuItem1.Text = "Копировать";
+            this.CopyStripMenuItem1.Click += new System.EventHandler(this.CopyStripMenuItem1_Click);
+            // 
+            // PasteStripMenuItem1
+            // 
+            this.PasteStripMenuItem1.Name = "PasteStripMenuItem1";
+            this.PasteStripMenuItem1.Size = new System.Drawing.Size(176, 26);
+            this.PasteStripMenuItem1.Text = "Вставить";
+            this.PasteStripMenuItem1.Click += new System.EventHandler(this.PasteStripMenuItem1_Click);
+            // 
+            // CutStripMenuItem1
+            // 
+            this.CutStripMenuItem1.Name = "CutStripMenuItem1";
+            this.CutStripMenuItem1.Size = new System.Drawing.Size(176, 26);
+            this.CutStripMenuItem1.Text = "Вырезать";
+            this.CutStripMenuItem1.Click += new System.EventHandler(this.CutStripMenuItem1_Click);
+            // 
+            // FontStripMenuItem1
+            // 
+            this.FontStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FontToolStripMenuItem1});
+            this.FontStripMenuItem1.Name = "FontStripMenuItem1";
+            this.FontStripMenuItem1.Size = new System.Drawing.Size(71, 24);
+            this.FontStripMenuItem1.Text = "Шрифт";
+            // 
+            // FontToolStripMenuItem1
+            // 
+            this.FontToolStripMenuItem1.Name = "FontToolStripMenuItem1";
+            this.FontToolStripMenuItem1.Size = new System.Drawing.Size(215, 26);
+            this.FontToolStripMenuItem1.Text = "Настроить шрифт";
+            this.FontToolStripMenuItem1.Click += new System.EventHandler(this.FontToolStripMenuItem1_Click);
+            // 
+            // InfoStripMenuItem1
+            // 
+            this.InfoStripMenuItem1.Name = "InfoStripMenuItem1";
+            this.InfoStripMenuItem1.Size = new System.Drawing.Size(62, 24);
+            this.InfoStripMenuItem1.Text = "Инфо";
+            this.InfoStripMenuItem1.Click += new System.EventHandler(this.InfoStripMenuItem1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CopyToolStripMenuItem1,
+            this.PasteToolStripMenuItem1,
+            this.CutToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 76);
+            // 
+            // CopyToolStripMenuItem1
+            // 
+            this.CopyToolStripMenuItem1.Name = "CopyToolStripMenuItem1";
+            this.CopyToolStripMenuItem1.Size = new System.Drawing.Size(162, 24);
+            this.CopyToolStripMenuItem1.Text = "Копировать";
+            this.CopyToolStripMenuItem1.Click += new System.EventHandler(this.CopyToolStripMenuItem1_Click);
+            // 
+            // PasteToolStripMenuItem1
+            // 
+            this.PasteToolStripMenuItem1.Name = "PasteToolStripMenuItem1";
+            this.PasteToolStripMenuItem1.Size = new System.Drawing.Size(162, 24);
+            this.PasteToolStripMenuItem1.Text = "Вставить";
+            this.PasteToolStripMenuItem1.Click += new System.EventHandler(this.PasteToolStripMenuItem1_Click);
+            // 
+            // CutToolStripMenuItem1
+            // 
+            this.CutToolStripMenuItem1.Name = "CutToolStripMenuItem1";
+            this.CutToolStripMenuItem1.Size = new System.Drawing.Size(162, 24);
+            this.CutToolStripMenuItem1.Text = "Вырезать";
+            this.CutToolStripMenuItem1.Click += new System.EventHandler(this.CutToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TextE";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem OpenStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem SaveStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem SaveAsStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem EditStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CopyStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem PasteStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CutStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FontStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FontToolStripMenuItem1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem CopyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem PasteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem InfoStripMenuItem1;
+    }
+}
+
---
+++ TextE/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace TextE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text File(*.txt)|*.txt";
+            Check();
+            
+        }
+
+        private void Check()
+        {
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            Regex regExp = new Regex(pattern);
+
+            foreach (Match match in regExp.Matches(richTextBox1.Text))
+            {
+                richTextBox1.Select(match.Index, match.Length);
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+        }
+        private void SaveAsStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            File.WriteAllText(filename, richTextBox1.Text);
+            MessageBox.Show("Файл сохранен!");
+        }
+        private void OpenStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string fileText = File.ReadAllText(filename);
+            richTextBox1.Text = fileText;
+
+            MessageBox.Show("Файл открыт!");
+        }
+
+        private void SaveStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// /////////////////////////////
+
+
+        private void CopyStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Copy();
+        }
+        private void PasteStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Paste();
+        }
+
+        private void CutStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Cut();
+        }
+
+        private void FontToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            richTextBox1.Font = fontDialog1.Font;
+        }
+
+        /// /////////////////////////////////////////
+
+        private void InfoStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            char[] ch = richTextBox1.Text.ToCharArray();
+            var dCount = ch.Where((n) => n >= '0' && n <= '9').Count();
+
+            int vCount = 0, cCount = 0;
+
+            foreach(char c in richTextBox1.Text)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    if (c == 'i' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                        vCount++;
+                    else
+                        if (c == 'I' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                        vCount++;
+                    else
+                        cCount++;
+                }
+                    
+            }
+
+            var authorPages = 1;
+
+            if(richTextBox1.Text.Length > 1800)
+                authorPages++;
+
+            var emptyLineCount = richTextBox1.Lines.Count(x => string.IsNullOrEmpty(x));
+            var lineCount = richTextBox1.Lines.Count();
+            var Bytes = richTextBox1.Text.Length * sizeof(Char);
+            MessageBox.Show("\nCharacter count is " + richTextBox1.Text.Length + "\nSize in Bytes " + Bytes + "\nLine number is " 
+                + lineCount.ToString() + "\nEmpty line " + emptyLineCount + "\nAuthor pages " + authorPages +
+                "\nNumber of Vowels is " + vCount + "\nNumber of Consonants is " + cCount +
+                "\nDigit number is " + dCount + "\nSpecial symbol is " + richTextBox1.Text.Count(char.IsPunctuation));
+        }
+
+        private void richTextBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                richTextBox1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void CopyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Copy();
+        }
+
+        private void PasteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Paste();
+        }
+
+        private void CutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+                richTextBox1.Cut();
+        }
+
+
+    }
+}
